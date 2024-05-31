c95bdd0e2ca5a113f18171550063ee847d99520d
antonstep <103253252+antonstep@users.noreply.github.com>
2024-05-31 01:17:40 +0000

    Fix default export in config.js

---
+++ src/config.js
@@ -0,0 +1,17 @@
+// src/config.js
+
+// Configuration for production environment
+const prod = {
+  API_URL: 'https://stock-analysis-backend-y6qh.onrender.com',
+};
+
+// Configuration for development environment
+const dev = {
+  API_URL: 'https://stock-analysis-backend-y6qh.onrender.com/api/data',
+};
+
+// Determine which configuration to export based on the environment
+const config = process.env.NODE_ENV === 'development' ? dev : prod;
+
+// Default export of the configuration
+export default config;
