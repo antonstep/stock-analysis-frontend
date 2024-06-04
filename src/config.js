7cc305f61ea3f625a80e2cb5a9b33eed5605e373
antonstep <103253252+antonstep@users.noreply.github.com>
2024-06-04 00:44:39 +0000

    Fix ESLint issues for CI build

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 // src/config.js
 
-const configuration = {
+// Configuration object
+const config = {
   API_URL: process.env.REACT_APP_BACKEND_URL || 'https://default-backend-url.com/api/data'
 };
 
-export default configuration;
+// Exporting the configuration object
+export default config;
