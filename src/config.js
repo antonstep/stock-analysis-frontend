b2bb697e0867893172d0f4b4a906b873412eff41
antonstep <103253252+antonstep@users.noreply.github.com>
2024-06-04 01:48:21 +0000

    connecting front and back

--- src/config.js
+++ src/config.js
@@ -1,9 +1,15 @@
-// src/config.js
-
-// Configuration object
-const config = {
-  API_URL: process.env.REACT_APP_BACKEND_URL
+// Configuration for production environment
+const prod = {
+  API_URL: 'https://stock-analysis-backend-y6qh.onrender.com',
 };
 
-// Assign the object to a variable before exporting
+// Configuration for development environment
+const dev = {
+  API_URL: 'https://stock-analysis-backend-y6qh.onrender.com/api/data',
+};
+
+// Determine which configuration to export based on the environment
+const config = process.env.NODE_ENV === 'development' ? dev : prod;
+
+// Default export of the configuration
 export default config;
