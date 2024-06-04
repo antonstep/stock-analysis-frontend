c8937c7b1ceaf004a77a5b37b0d1b893cf5343d3
antonstep <103253252+antonstep@users.noreply.github.com>
2024-06-04 00:40:26 +0000

    connecting front and back

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
-// Configuration for all environments
-const API_URL = process.env.REACT_APP_BACKEND_URL || 'https://stock-analysis-backend-y6qh.onrender.com/api/data';
+const configuration = {
+  API_URL: process.env.REACT_APP_BACKEND_URL || 'https://default-backend-url.com/api/data'
+};
 
-// Export the configuration
-export default { API_URL };
+export default configuration;
